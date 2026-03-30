fef407498a8e7da47f447d7f565a43af03b388bf
Taliya <taliya0127@gmail.com>
2026-03-30 20:42:12 +0530

    Initial commit

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,10 @@
+package com.mvit;
+
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Maven project executed successfully!");
+    }
+}
